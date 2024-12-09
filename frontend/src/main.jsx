47df4bb7b525acf7bdcf867b5dd91391d29dfd17
antonux <marcusremigoso@gmail.com>
2024-12-09 12:40:35 +0800

    + installed font

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
+import '@fontsource/inter'; 
+import '@fontsource/inter/100.css'; import '@fontsource/inter/200.css'; import '@fontsource/inter/300.css'; import '@fontsource/inter/500.css'; import '@fontsource/inter/600.css'; import '@fontsource/inter/800.css'; import '@fontsource/inter/900.css';
+
 
 //contexts
 import { SidebarProvider } from "./contexts/SideBarContext";
