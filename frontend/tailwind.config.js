2a081f8b31fabc27df8dbb027ce927bf7d7098b0
antonux <marcusremigoso@gmail.com>
2025-01-10 23:04:26 +0800

    fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,17 @@
+import tailwindScrollbar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'], // Adds Inter as the primary sans font
+        sans: ["Inter", "sans-serif"], // Adds Inter as the primary sans font
       },
-       transitionProperty: {
-        width: 'width',
+      transitionProperty: {
+        width: "width",
       },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
-}
+  plugins: [tailwindScrollbar],
+};
