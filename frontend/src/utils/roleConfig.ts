583f47b869606e7ca5ebab94138c370fbaa0c220
antonux <marcusremigoso@gmail.com>
2025-01-12 23:55:45 +0800

    bump sidebar

--- frontend/src/utils/roleConfig.ts
+++ frontend/src/utils/roleConfig.ts
@@ -7,43 +7,35 @@ import * as VscIcons from 'react-icons/vsc';
 const roleConfig = {
   admin: {
     sidebarLinks: [
-      { name: "Overview", path: "/overview", icon: Icons.MdOutlineDashboard },
       { name: "Inventory", path: "/inventory", icon: Icons.MdOutlineInventory2 },
       { name: "Request", path: "/request", icon: VscIcons.VscGitPullRequest },
       { name: "Purchase", path: "/purchase", icon: FaIcons.FaMoneyBill },
       { name: "Suppliers", path: "/suppliers", icon: FaIcons.FaBoxes },
       { name: "Accounts", path: "/accounts", icon: FaIcons.FaUsers },
       { name: "Notification", path: "/notifications", icon: IoIcons.IoMdNotifications },
-      { name: "Settings", path: "/settings", icon: BiIcons.BiCog },
     ],
   },
   store: {
     sidebarLinks: [
-      { name: "Overview", path: "/overview", icon: Icons.MdOutlineDashboard },
       { name: "Inventory", path: "/inventory", icon: Icons.MdOutlineInventory2 },
       { name: "Request", path: "/request", icon: VscIcons.VscGitPullRequest },
       { name: "Notification", path: "/notifications", icon: IoIcons.IoMdNotifications },
-      { name: "Settings", path: "/settings", icon: BiIcons.BiCog },
     ],
   },
   manager: {
     sidebarLinks: [
-      { name: "Overview", path: "/overview", icon: Icons.MdOutlineDashboard },
       { name: "Inventory", path: "/inventory", icon: Icons.MdOutlineInventory2 },
       { name: "Request", path: "/request", icon: VscIcons.VscGitPullRequest },
       { name: "Notification", path: "/notifications", icon: IoIcons.IoMdNotifications },
-      { name: "Settings", path: "/settings", icon: BiIcons.BiCog },
     ],
   },
   warehouse: {
     sidebarLinks: [
-      { name: "Overview", path: "/overview", icon: Icons.MdOutlineDashboard },
       { name: "Inventory", path: "/inventory", icon: Icons.MdOutlineInventory2 },
       { name: "Request", path: "/request", icon: VscIcons.VscGitPullRequest },
       { name: "Purchase", path: "/purchase", icon: FaIcons.FaMoneyBill },
       { name: "Suppliers", path: "/suppliers", icon: FaIcons.FaBoxes },
       { name: "Notification", path: "/notifications", icon: IoIcons.IoMdNotifications },
-      { name: "Settings", path: "/settings", icon: BiIcons.BiCog },
     ],
   },
 };
